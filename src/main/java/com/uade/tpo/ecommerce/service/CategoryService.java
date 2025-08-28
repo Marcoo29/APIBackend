83b7eb1324674a92d3f7b415bd69ef6806c4adbe
Ale Fernando <fale@labolsa.com.ar>
2025-08-28 03:01:28 -0300

    endpoints de get, post y getcategorybyid

--- src/main/java/com/uade/tpo/ecommerce/service/CategoryService.java
+++ src/main/java/com/uade/tpo/ecommerce/service/CategoryService.java
@@ -1,5 +1,17 @@
 package com.uade.tpo.ecommerce.service;
 
-public class CategoryService {
-    
+import java.util.Optional;
+
+import com.uade.tpo.ecommerce.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.uade.tpo.ecommerce.exceptions.CategoryDuplicateException;
+
+public interface CategoryService {
+    public Page<Category> getCategories(PageRequest pageRequest);
+
+    public Optional<Category> getCategoryById(Long categoryId);
+
+    public Category createCategory(String description) throws CategoryDuplicateException;
 }
---
+++ src/main/java/com/uade/tpo/ecommerce/service/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.uade.tpo.ecommerce.service;
+
+import java.util.Optional;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.uade.tpo.ecommerce.exceptions.CategoryDuplicateException;
+import com.uade.tpo.ecommerce.repository.CategoryRepository;
+import com.uade.tpo.ecommerce.entity.Category;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Page<Category> getCategories(PageRequest pageable) {
+        return categoryRepository.findAll(pageable);
+    }
+
+    public Optional<Category> getCategoryById(Long categoryId) {
+        return categoryRepository.findById(categoryId);
+    }
+
+    public Category createCategory(String description) throws CategoryDuplicateException {
+        List<Category> categories = categoryRepository.findByDescription(description);
+        if (categories.isEmpty())
+            return categoryRepository.save(new Category(description));
+        throw new CategoryDuplicateException();
+    }
+}
