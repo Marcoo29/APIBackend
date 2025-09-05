2ba44761d94ebf27a0f61a72da98b272f3103b74
rodrigobatalla <rodrigombatalla@gmail.com>
2025-09-04 21:08:27 -0300

    Se agregaron los metodos getAllUsers, disableUser y updateUser.

--- src/main/java/com/uade/tpo/ecommerce/controllers/UserController.java
+++ src/main/java/com/uade/tpo/ecommerce/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.uade.tpo.ecommerce.controllers;
 
 import java.net.URI;
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.uade.tpo.ecommerce.entity.User;
 import com.uade.tpo.ecommerce.entity.dto.UserRequest;
+import com.uade.tpo.ecommerce.entity.enums.UserStatus;
 import com.uade.tpo.ecommerce.exceptions.UserDuplicateException;
 import com.uade.tpo.ecommerce.service.inter.UserService;
 
@@ -48,4 +51,50 @@ public class UserController {
         return ResponseEntity.created(URI.create("/users/" + result.getId())).body(result);
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+
+        if (users.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @PatchMapping("/{userId}/update")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody UserRequest userRequest){
+        Optional<User> userOptional = userService.getUserById(userId);
+
+        if(userOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = userOptional.get();
+        user.setUsername(userRequest.getUsername());
+        user.setName(userRequest.getName());
+        user.setLastname(userRequest.getLastname());
+        user.setAddress(userRequest.getAddress());
+        user.setRole(userRequest.getRole());
+
+        User updatedUser = userService.updateUser(user);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PatchMapping("/{userId}/disable")
+    public ResponseEntity<User> disableUser(@PathVariable Long userId){
+        Optional<User> userOptional = userService.getUserById(userId);
+
+        if(userOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = userOptional.get();
+        user.setStatus(UserStatus.INACTIVE);
+
+        User disabledUser = userService.updateUser(user); // reutilizo el metodo updateUser para grabar los cambios
+
+        return ResponseEntity.ok(disabledUser);
+    }
+    
 }
