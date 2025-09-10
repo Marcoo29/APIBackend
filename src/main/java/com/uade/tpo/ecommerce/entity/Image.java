72c43e9aa9259a68f17519c563c4761759650d30
Marcoo29 <marco.legaspi05@gmail.com>
2025-09-10 17:04:55 -0300

    Carga de imagenes

---
+++ src/main/java/com/uade/tpo/ecommerce/entity/Image.java
@@ -0,0 +1,29 @@
+package com.uade.tpo.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "image_table")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Blob image;
+}
+
